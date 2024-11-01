d65acf6547404475c63f407791409f6367e12bcd
Naveed Ali <naveedalilodro.xwave@gmail.com>
2024-11-01 10:30:59 -1200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import TodoList from "./todolist"; 
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min";
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <TodoList />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/todolist.jsx
@@ -0,0 +1,61 @@
+import React, { useState } from "react";
+
+const TodoList = () => {
+  const [inputValue, setInputValue] = useState("");
+  const [todoApp, setTodoApp] = useState([]);
+
+  const formHandler = (e) => {
+    e.preventDefault();
+    if (inputValue.trim()) {
+      setTodoApp((prev) => [...prev, inputValue.trim()]);
+      setInputValue("");
+    }
+  };
+
+  const deleteHandler = (task) => {
+    const removedTask = todoApp.filter((list) => list !== task);
+    setTodoApp(removedTask);
+  };
+
+  return (
+    <div className="my-5 d-flex justify-content-center">
+      <div style={{ width: "600px" }} className="bg-warning rounded-3 text-white p-3 shadow">
+        <h1 className="text-center mb-4 text-black">To-Do List</h1>
+        <form className="d-flex mb-3" onSubmit={formHandler}>
+          <input
+            value={inputValue}
+            className="form-control py-2 rounded"
+            type="text"
+            id="taskInput"
+            placeholder="Add your task..."
+            onChange={(e) => setInputValue(e.target.value)}
+          />
+          
+          <button className="btn bg-primary text-white w-25 ms-2" type="submit">
+            Add Task
+          </button>  
+          
+        </form>
+        <div className="rounded-3">
+          {todoApp.map((task, index) => (
+            <div
+              className="second-input d-flex justify-content-between align-items-center p-2"
+              style={{ borderBottom: "1px solid #ccc" }}
+              key={index}
+            >
+
+
+
+              <h5>{task}</h5>
+              <button onClick={() => deleteHandler(task)} className="btn btn-danger">
+                Delete
+              </button>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default TodoList;
